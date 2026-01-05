29e53b77c32fb2e2bceb04d6ee76f35e69e72f25
tanzz <bustanuldarking23@gmail.com>
2026-01-05 20:59:49 +0800

    membuat authorization api dan setup deploy vercel

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,28 @@
 import express from "express";
 import router from "./routes/api";
+import bodyParser from "body-parser";
+import db from "./utils/database";
 
-const app = express();
+async function init() {
+  try {
+    // memastikan db terkonek
+    const result = await db();
+    console.log("database status: ", result);
 
-const PORT = 3001;
+    const app = express();
+    app.use(bodyParser.json());
 
-// middleware untuk path url dengan pola /api maka akan mengarah ke variabel router
-app.use("/api", router);
+    const PORT = 3001;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+    // middleware untuk path url dengan pola /api maka akan mengarah ke variabel router
+    app.use("/api", router);
 
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+init();
