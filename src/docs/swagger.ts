0fee27bedddbd9e8a3d3b9a7095faee7262c6db7
tanzz <bustanuldarking23@gmail.com>
2026-01-05 21:59:21 +0800

    feat: add api documentation for login and register

---
+++ src/docs/swagger.ts
@@ -0,0 +1,38 @@
+import swaggerAutogen from "swagger-autogen";
+
+const doc = {
+  info: {
+    version: "v0.0.1",
+    title: "Dokumentasi API Event APP",
+    description: "Dokumentasi API Event APP",
+  },
+  servers: [
+    {
+      url: "http://localhost:3001/api",
+      description: "Local Server",
+    },
+    {
+      url: "https://be-event-app-one.vercel.app/",
+      description: "Deploy Server",
+    },
+  ],
+  components: {
+    securitySchemes: {
+      bearerAuth: {
+        type: "http",
+        scheme: "bearer",
+      },
+    },
+    schemas: {
+      LoginRequest: {
+        identifier: "Tanzz",
+        password: "bustanul_089",
+      },
+    },
+  },
+};
+
+const outputFile = "./swagger_output.json";
+const endpointsFiles = ["../routes/api.ts"];
+
+swaggerAutogen({ openapi: "3.0.0" })(outputFile, endpointsFiles, doc);
--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,20 @@ import express from "express";
 import router from "./routes/api";
 import bodyParser from "body-parser";
 import db from "./utils/database";
+import docs from "./docs/route";
+import cors from "cors";
 
 async function init() {
   try {
+    const PORT = 3001;
     // memastikan db terkonek
     const result = await db();
     console.log("database status: ", result);
 
     const app = express();
-    app.use(bodyParser.json());
 
-    const PORT = 3001;
+    app.use(cors());
+    app.use(bodyParser.json());
 
     app.get("/", (req, res) => {
       res.status(200).json({
@@ -23,6 +26,7 @@ async function init() {
 
     // middleware untuk path url dengan pola /api maka akan mengarah ke variabel router
     app.use("/api", router);
+    docs(app);
 
     app.listen(PORT, () => {
       console.log(`Server is running on http://localhost:${PORT}`);
