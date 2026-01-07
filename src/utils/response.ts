237adf5d525b4835c2b3eab754bbd900b3c46019
tanzz <bustanuldarking23@gmail.com>
2026-01-07 21:05:57 +0800

    feat: new API Location

--- src/utils/response.ts
+++ src/utils/response.ts
@@ -1,5 +1,5 @@
-import { json } from "body-parser";
 import { Response } from "express";
+import mongoose from "mongoose";
 import * as Yup from "yup";
 
 type Pagination = {
@@ -26,9 +26,40 @@ export default {
           status: 400,
           message,
         },
-        data: error.errors,
+        data: {
+          [`${error.path}`]: error.errors[0],
+        },
       });
     }
+
+    if (error instanceof mongoose.Error) {
+      return res.status(500).json({
+        meta: {
+          status: 500,
+          message: error.message,
+        },
+        data: error.name,
+      });
+    }
+
+    if ((error as any)?.code) {
+      const _err = error as any;
+      return res.status(500).json({
+        meta: {
+          status: 500,
+          message: _err.errorResponse.errmsg,
+        },
+        data: _err,
+      });
+    }
+
+    res.status(500).json({
+      meta: {
+        status: 500,
+        message,
+      },
+      data: error,
+    });
   },
 
   unathorized(res: Response, message: string = "unauthorized") {
