f305b61c0d9a2c7807c2d84d8e35c3c15ef9aba3
tanzz <bustanuldarking23@gmail.com>
2026-01-11 16:55:59 +0800

    fix: find event filter

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -291,7 +291,34 @@ router.post(
 router.get(
   "/events",
   eventController.findAll
-  /* #swagger.tags = ['Events'] */
+  /* #swagger.tags = ['Events']
+    #swagger.parameters['limit'] = {
+    in: 'query',
+    type: 'number',
+    default: 10
+    }
+    #swagger.parameters['page'] = {
+    in: 'query',
+    type: 'number',
+    default: 1
+    }
+    #swagger.parameters['category'] = {
+    in: 'query',
+    type: 'string',
+    }
+    #swagger.parameters['isOnline'] = {
+    in: 'query',
+    type: 'boolean',
+    }
+    #swagger.parameters['isPublish'] = {
+    in: 'query',
+    type: 'boolean',
+    }
+    #swagger.parameters['isFeatured'] = {
+    in: 'query',
+    type: 'boolean',
+    }
+  */
 );
 
 router.get(
